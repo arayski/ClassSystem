68a3d6661e2b6a66f4f866ce34f4195dd631ae10
Nathan <nathan.coleman@uconn.edu>
2023-11-01 09:35:37 -0400

    added styling and formatting to homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,24 @@
 <template>
   <main class="home">
     <h2>My Schedule</h2>
-    <p>
-      PlaceHolder Classes
-    </p>
-    <ul>
-      <li>Class 1</li>
-      <li>Instructor</li>
-      <li>Time</li>
-      <li>Location</li>
-    </ul>
-    <br>
-    <ul>
-      <li>Class 2</li>
-      <li>Instructor</li>
-      <li>Time</li>
-      <li>Location</li>
-    </ul>
-    <br>
-    <ul>
-      <li>Class 3</li>
-      <li>Instructor</li>
-      <li>Time</li>
-      <li>Location</li>
-    </ul>
-    <br>
-    <ul>
-      <li>Class 4</li>
-      <li>Instructor</li>
-      <li>Time</li>
-      <li>Location</li>
-    </ul>
-
-    
-    
+    <div class="box-container">
+      <div class="box">
+        <img src="enrollment-icon.png" alt="Class Enrollment" />
+        <p>Class Enrollment</p>
+      </div>
+      <div class="box">
+        <img src="search-icon.png" alt="Class Search" />
+        <p>Class Search</p>
+      </div>
+      <div class="box">
+        <img src="schedule-icon.png" alt="View Schedule" />
+        <p>View Schedule</p>
+      </div>
+      <div class="box">
+        <img src="profile-icon.png" alt="User Profile" />
+        <p>User Profile</p>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -48,7 +33,28 @@
   color: #000E2F;
 }
 
-.home p {
+.box-container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 2rem;
+}
+
+.box {
+  text-align: center;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  width: 23%;
+}
+
+.box img {
+  width: 80px;
+  height: 80px;
   margin-bottom: 1rem;
+}
+
+.box p {
+  font-size: 18px;
+  color: #333;
 }
 </style>
